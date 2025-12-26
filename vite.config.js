09dce486b3fc7e2761bc45e5292bffe2df3d219c
Yahya <yahyajaan192@gmail.com>
2025-12-26 18:57:01 +0500

    Update package.json, vite.config.js, and package-lock.json for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    react(),      // Enables React fast refresh and JSX support
+    tailwindcss(), // Integrates TailwindCSS
   ],
+  base: '/payroll.raideit.com/', // Required for GitHub Pages deployment
 })
